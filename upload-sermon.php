9ec7db7be1b32a4bc44435b1bd7c281171640cd9
Sid Bachtiar <sid.bachtiar@gmail.com>
2020-07-18 22:38:49 +1200

    WIP

--- upload-sermon.php
+++ upload-sermon.php
@@ -50,19 +50,44 @@ if (!isset($_POST['submit'])) { ?>
 // require wp-load.php to use built-in WordPress functions
 require_once("../wp-load.php");
 
-// // these fields can't be empty
-// foreach (['title', 'date', 'upload_id', 'submit'] as $f) {
-//     if (empty($_POST[$f])) {
-//         echo "<a href=''>Ooops! something is wrong, try again!</a>";
-//         exit();
-//     }
-// }
+// these fields can't be empty
+foreach (['title', 'date', 'upload_id', 'submit'] as $f) {
+    if (empty($_POST[$f])) {
+        echo "<a href=''>Ooops! something is wrong, try again!</a>";
+        exit();
+    }
+}
+
+// these file(s) must be uploaded
+if (!isset($_FILES['audio'])) {
+    echo "<a href=''>Ooops! something is wrong, try again!</a>";
+    exit();
+}
+
+// type must be audio
+if (substr($_FILES['audio']['type'], 0, 6) !== 'audio/') {
+    echo "<a href=''>Ooops! something is wrong, try again!</a>";
+    exit();
+}
 
 $upload_dir = __DIR__."/../wp-content/uploads/";
 
+$base_file_name = preg_replace("/[^A-Za-z0-9\\-\\.]/", "_", trim($_FILES['audio']['name']));
+$path_info = pathinfo($file_name);
+$file_name = $path_info['filename'].'.'.$path_info['extension'];
+
+$i = 1;
+while (file_exists($upload_dir.'/'.$file_name)) {
+    $i++;
+    $file_name = $path_info['filename'].'-'.$i.'.'.$path_info['extension'];
+}
+
 echo "<pre>";
-print_r($_POST);
-print_r($_FILES);
+echo "$file_name\n";
+// print_r($_POST);
+// print_r($_FILES);
 echo "</pre>";
+
+
 
 echo "Successful!";
