05c06afb76a66f4068ccfcbe4897c3346f96d396
Sid Bachtiar <sid.bachtiar@gmail.com>
2020-07-18 21:42:10 +1200

    WIP

--- upload-sermon.php
+++ upload-sermon.php
@@ -1,5 +1,16 @@
 <?php 
 
-require_once 'config.php' or die("Config not found!");
+require_once 'config.php';
+
+$user = $_SERVER['PHP_AUTH_USER'];
+$pass = $_SERVER['PHP_AUTH_PW'];
+
+$validated = $user === AUTH_LOGIN && $pass === AUTH_PASS;
+
+if (!$validated) {
+  header('WWW-Authenticate: Basic realm="Please login first"');
+  header('HTTP/1.0 401 Unauthorized');
+  die ("Not authorized");
+}
 
 echo "Hello World";
