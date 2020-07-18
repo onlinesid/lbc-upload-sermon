c7202f9f1b75a994e45e147d2ebdda73eb983787
Sid Bachtiar <sid.bachtiar@gmail.com>
2020-07-18 21:48:57 +1200

    WIP

--- upload-sermon.php
+++ upload-sermon.php
@@ -13,4 +13,26 @@ if (!$validated) {
   die ("Not authorized");
 }
 
-echo "Hello World";
+// display the upload form
+if (!isset($_POST['upload'])) { ?>
+
+    <html>
+        <head>
+            <title>Upload sermon</title>
+        </head>
+        <body>
+            <form action="" method="post">
+                <p>
+                    <label>Label:</label>
+                    <input type="text" name="label" value="Matthews 5 (Dave) <?=date('d-M')?>" />
+                </p>
+            </form>
+        </body>
+    </html>
+
+    <?php exit();
+}
+
+// require wp-load.php to use built-in WordPress functions
+require_once("../wp-load.php");
+
